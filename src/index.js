e532775b53fd57f061c9568a5a08e8d9b999c323
notdavo <da.arce.97@gmail.com>
2024-11-22 21:47:42 -0600

    Added CSS to the elements

--- src/index.js
+++ src/index.js
@@ -1,34 +1,65 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { pizzaData } from "./data";
+import "./index.css";
 
 function App() {
   return (
-    <div>
-      <h1>Hello React</h1>
-      <Pizza />
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
     </div>
   );
 }
 
-function Pizza() {
+function Header() {
   return (
-    <>
-      {pizzaData.map((pizza) => {
-        const nameForImage = pizza.name.match(/(?:\w+\s)?(\w+)/);
+    <header className="header">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
+}
 
-        return (
-          <div key={pizza.name}>
-            <img
-              src={`pizzas/${nameForImage[1].toLowerCase()}.jpg`}
-              alt={nameForImage}
-            />
-            <h2>{pizza.name}</h2>
-            <p>{pizza.ingredients}</p>
-          </div>
-        );
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>Our menu</h2>
+      {pizzaData.map((pizza) => {
+        return <Pizza key={pizza.name} pizza={pizza} />;
       })}
-    </>
+    </main>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+
+  return (
+    <footer className="footer">
+      {new Date().toLocaleDateString()}. We're currently open
+    </footer>
+  );
+}
+
+function Pizza(props) {
+  const pizzaNameForImage = props.pizza.name.match(/(?:\w+\s)?(\w+)$/);
+  return (
+    <div className="pizza">
+      <img
+        src={`pizzas/${pizzaNameForImage[1].toLowerCase()}.jpg`}
+        alt={pizzaNameForImage}
+      />
+      <div>
+        <h3>{props.pizza.name}</h3>
+        <p>{props.pizza.ingredients}</p>
+        <spa>${props.pizza.price}</spa>
+      </div>
+    </div>
   );
 }
 
